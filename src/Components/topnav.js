5555cd91c674b32151e4c943fd202243bd7078d3
Amy Shan <amyshan@Amys-MacBook-Pro.local>
2017-10-10 10:08:52 -0400

    added basic navbar

--- src/Components/topnav.js
+++ src/Components/topnav.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import { Button,
+         FormControl,
+         Glyphicon,
+         InputGroup,
+         Navbar,
+         Col,
+         Nav
+} from 'react-bootstrap';
 
 export default class TopNav extends Component {
     render() {
         return (
-            <div id="topnav">
-                <p>topnav will go here</p>
-            </div>
+            <Navbar >
+                <Col xs={7}>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a>org name</a>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                </Col>
+                <Col xs={5}>
+                    <Nav pullRight>
+                        <Navbar.Form pullRight>
+                            <InputGroup>
+                                <FormControl type="text" placeholder="Search" />
+                                <InputGroup.Button>
+                                    <Button><Glyphicon glyph="search"/></Button>
+                                </InputGroup.Button>
+                            </InputGroup>
+                        </Navbar.Form>
+                    </Nav>
+              </Col>
+            </Navbar>
         );
     }
 }
